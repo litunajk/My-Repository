e2ed081b1ac6a6b20b0dbd71f9e144ad98188705
litunajk <litunajk47@gmail.com>
2017-03-08 17:33:49 +0530

    finish
    just added

---
+++ PrimsTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimsTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "‪F:\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://primusbank.qedgetech.com");
+	}
+
+}
